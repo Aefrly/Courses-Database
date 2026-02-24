0090be5757b123ac2179124e2ec0d7147b8f1e9d
Aefrly <fraley.allison@ic.edu>
2026-02-23 19:01:41 -0600

    Commented out module export fuction command

--- database/setup.js
+++ database/setup.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 //Create function to setup database
-module.exports = function setupDatabase() {
+//module.exports = function setupDatabase() {
   // Create/connect to database file
   const db = new sqlite3.Database('./database/college-courses.db');
   console.log('Connected to SQLite database');
@@ -20,4 +20,4 @@ module.exports = function setupDatabase() {
   `), () => {
     console.log('Courses table created');
     };
-};
+//};
