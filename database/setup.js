d3d1c1bdd23a6e48b1a0c846132bcf7a23dcc1d1
Aefrly <fraley.allison@ic.edu>
2026-02-23 19:21:39 -0600

    Fixed my indentation

--- database/setup.js
+++ database/setup.js
@@ -1,23 +1,20 @@
 //Export package
 const sqlite3 = require('sqlite3').verbose();
 
-//Create function to setup database
-//module.exports = function setupDatabase() {
-  // Create/connect to database file
-  const db = new sqlite3.Database('./database/college-courses.db');
-  console.log('Connected to SQLite database');
+// Create/connect to database file
+const db = new sqlite3.Database('./database/college-courses.db');
+console.log('Connected to SQLite database');
 
-  // Create courses table
-  db.run(`
-    CREATE TABLE IF NOT EXISTS courses (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      courseCode TEXT,
-      title TEXT,
-      credits INTEGER,
-      description TEXT,
-      semester TEXT
-    )
-  `, () => {
-    console.log('Courses table created');
-  });
-//};
+// Create courses table
+db.run(`
+  CREATE TABLE IF NOT EXISTS courses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    courseCode TEXT,
+    title TEXT,
+    credits INTEGER,
+    description TEXT,
+    semester TEXT
+  )
+`, () => {
+  console.log('Courses table created');
+});
